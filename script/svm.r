b7f5060cc95c023420ad66b562c4f7cd27fcac95
zyuqian <zhangyuqian97@gmail.com>
2019-12-08 20:14:16 +0800

    modify svm script

--- script/svm.r
+++ script/svm.r
@@ -1,58 +1,54 @@
 library(e1071)
+library(dplyr)
 
-df <- read.csv(file="Everything_noNA_noID.csv", header=TRUE, sep=",")
+dta <- read.csv(file="Everything_noNA_noID.csv", header=TRUE, sep=",")
 
-# remove unusable columns
-df$ff012_3 <- NULL
-df$fl020s6 <- NULL
-df$which.knee <- NULL
+df <- select(dta, xsiblingnum, ca000_w3_2_1_, fa001, cc012_w3_1_, ge010_6, Stock.investment, ca001_w3_2_1_, hc039_w3, hand.strength.test.right.2, Water.cigarettes, knee.height, ge004, hd005_w3, i011 , Systolic.2 , cg003_w2_1_, fa006, breath.test.1, waist.circumference,cancer_true)
 
-# divde data into train and test
-index <- sample(2,nrow(df),replace = TRUE,prob=c(0.7,0.3))
-traindata <- df[index==1,]
-testdata <- df[index==2,]
+df$X <- NULL
+df$cancer_true = as.factor(df$cancer_true)
 
-# set up svm model
-svm_model <- svm(cancer_true~.,data=traindata, type="C")
-svm_model
+set.seed(2)
+train=sample(nrow(df), round(0.7*nrow(df)))
 
-# predict on training data
-svm_pred_1 <- predict(svm_model,traindata[,-121])
-table_1 <- table(pred=svm_pred_1,true=traindata[,121])
-table_1
+# set error.fun for fine tune
+mfn = function(real, pred){
+	good = real[real==TRUE] == pred[real==TRUE]
+	return(length(good[good==FALSE]))
+}
 
-accuracy_1 <- sum(diag(table_1))/sum(table_1)
-accuracy_1
+# set tune control
+tc = tune.control(cross=5, error.fun=mfn)
 
-# predict on test data
-svm_pred_2 <- predict(svm_model,testdata[,-121])
-table_2 <- table(pred=svm_pred_2,true=testdata[,121])
-table_2
+### polynomial kernel
 
-accuracy_2 <- sum(diag(table_2))/sum(table_2)
-accuracy_2
+# svmfit using best parameters that minimizes misclassification
+svmfit=svm(cancer_true~., data=df[train,], kernel="polynomial", degree=2, cost=1)
+summary(svmfit)
 
-# generate subset
-subdf <- select(df, hc039_w3, hd005_w3, hd003_004_w3, ge010_6, ca013_2_, xsiblingnum, cc001_w3s1, cc012_w3_1_, i011, ea006_1_, ea006_4_, ea006_10_, xrtype, fa006, ff012_1, fl022_1, Stock.investment, Systolic.2, breath.test.1, hand.strength.test.right.2, knee.height, waist.circumference, cancer_true)
+table(true=df[-train,"cancer_true"], pred=predict(svmfit, newdata=df[-train ,]))
 
-# repeat the process with all columns
-index <- sample(2,nrow(subdf),replace = TRUE,prob=c(0.7,0.3))
-subtrain <- subdf[index==1,]
-subtest <- subdf[index==2,]
+# fine tune to minimize false negative
+# to minimize misclassification, remove the parameter tunecontrol
+tune.out=tune(svm, cancer_true~.,data=df[train,],kernel="polynomial",ranges=list(cost=c(5000, 10000, 20000), degree=c(2,3,4)), tunecontrol=tc)
+summary(tune.out)
 
-svm_model_sub <- svm(cancer_true~.,data=subtrain, type="C")
-svm_model_sub
+# confusion matrix of best model after tuning
+table(true=df[-train,"cancer_true"], pred=predict(tune.out$best.model, newdata=df[-train ,]))
 
-svm_pred_1_sub <- predict(svm_model_sub,subtrain[,-23])
-table_1_sub <- table(pred=svm_pred_1_sub,true=subtrain[,23])
-table_1_sub
 
-accuracy_1_sub <- sum(diag(table_1_sub))/sum(table_1_sub)
-accuracy_1_sub
+### radial kernel
 
-svm_pred_2_sub <- predict(svm_model_sub,subtest[,-23])
-table_2_sub <- table(pred=svm_pred_2_sub,true=subtest[,23])
-table_2_sub
+# svmfit using best parameters that minimizes misclassification
+svmfit=svm(cancer_true~., data=df[train,], kernel="radial", gamma=0.001, cost=1000)
+summary(svmfit)
 
-accuracy_2_sub <- sum(diag(table_2_sub))/sum(table_2_sub)
-accuracy_2_sub
+table(true=df[-train,"cancer_true"], pred=predict(svmfit, newdata=df[-train ,]))
+
+# fine tune to minimize false negative
+# to minimize misclassification, remove the parameter tunecontrol
+tune.out=tune(svm, cancer_true~., data=df[train,], kernel="radial", ranges=list(cost=c(1e4, 5e4, 1e5), gamma=c(0.001, 0.005, 0.01)), tunecontrol=tc)
+summary(tune.out)
+
+# confusion matrix of best model after tuning
+table(true=df[-train,"cancer_true"], pred=predict(tune.out$best.model, newdata=df[-train ,]))

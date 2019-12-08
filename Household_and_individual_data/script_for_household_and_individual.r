cc20de90c32b4044944ebb5b9110a5a08987bfa3
Chelsea3 <yutonghk@hotmail.com>
2019-12-08 23:20:50 +0800

    12.8-update1

--- Household_and_individual_data/script_for_household_and_individual.r
+++ Household_and_individual_data/script_for_household_and_individual.r
@@ -83,6 +83,7 @@ write_csv(e_nona,"Sorted data/Everything_noNA.csv")
 library(readr)
 library(leaps)
 library(MASS)
+library(glmnet)
 set.seed(2)
 df<- read_csv("everthing_nona_purified.csv")
 
@@ -98,18 +99,29 @@ Y = as.matrix(df_train[1])
 X = as.matrix(df_train[-c(1,69,120)])
 
 full.model <- lm(Y~X)
+glm.try = glmnet(X,Y, family="binomial")
+summary(glm.try)
+
+pfit = predict(glm.try, X, s =0.001, type = "response")
+pred <- ifelse(pfit > 0.3, "TRUE", "FALSE")
+plot(Y, pfit)
+table(Y, pred)
+
+pfit = predict(glm.try, as.matrix(df_test[-c(1,69,120)]), s = 0.001, type = "response")
+plot(df_test[1], pfit)
+
 # stepwise selection
-step.model <- stepAIC(full.model, direction = "backward", trace = TRUE)
+step.model <- stepAIC(glm.try, direction = "forward", trace = FALSE)
 summary(step.model)
 plot(x=Y, y=step.model$fitted.values)
 mean((Y - step.model$fitted.values)^2)
 ###############################################################
-# using direction = "both""backward" "forward" result
+# using direction = "both""backward""forward" result
 # unscaled
 both_feature = c("hc039_w3","hd005_w3","hd003_004_w3","ge010_6","ca013_2_","xsiblingnum",
-         "cc001_w3s1","cc012_w3_1_","i011","ea006_1_","ea006_4_",
-         "fa006","ff012_1","fl022_1","Stock.investment","Systolic.2","breath.test.1",
-         "hand.strength.test.right.2","knee.height","waist.circumference")
+                 "cc001_w3s1","cc012_w3_1_","i011","ea006_1_","ea006_4_",
+                 "fa006","ff012_1","fl022_1","Stock.investment","Systolic.2","breath.test.1",
+                 "hand.strength.test.right.2","knee.height","waist.circumference")
 # scaled
 both_feature = c("hc001","hc039_w3","hd005_w3","ge004","ge010_6","ca000_w3_2_1_","ca001_w3_2_1_","xsiblingnum",
                  "cc001_w3s1","cc012_w3_1_","i011","i020","i021","ea006_2_","ea006_4_","ea006_10_","xrtype",
@@ -153,22 +165,93 @@ normalized<-function(y) {
 }
 #############################################################
 # use logistic regression to see error
-'''
-both_df <- cbind(Y,df[both_feature])
+# 
+# both_df <- cbind(Y,df[both_feature])
+# 
+# both_df2 <- as.data.frame(both_df[1])
+# for (i in c(2:23)) {
+#   both_df2 <- cbind(both_df2, normalized(both_df[i]))
+# }
+# 
+# both_df2 <- scale(both_df, center = TRUE,scale = TRUE)
+# 
+# glm.fits_both = glm(cancer_true~., data=both_df2, family =binomial)
+# summary(glm.fits_both)
+# glm.prob = predict(glm.fits_both,both_df2,type="response")
+# glm.pred <- ifelse(glm.prob>0.5, TRUE, FALSE)
+# table(glm.pred, Y)
 
-both_df2 <- as.data.frame(both_df[1])
-for (i in c(2:23)) {
-  both_df2 <- cbind(both_df2, normalized(both_df[i]))
-}
-
-both_df2 <- scale(both_df, center = TRUE,scale = TRUE)
-
-glm.fits_both = glm(cancer_true~., data=both_df2, family =binomial)
-summary(glm.fits_both)
-glm.prob = predict(glm.fits_both,both_df2,type="response")
-glm.pred <- ifelse(glm.prob>0.5, TRUE, FALSE)
-table(glm.pred, Y)
-'''
 #regfit.full=regsubsets(x=as.matrix(df[-c(1:4,127)]), y=as.matrix(df[127]))
 #regfit.fwd = regsubsets(y=as.matrix(df[127]), x=as.matrix(df[-c(1:4,127)]), nvmax=20, method="forward")
 #summary (regfit.fwd)
+
+#--------------12.7 update------------
+library(readr)
+library(leaps)
+library(MASS)
+library(glmnet)
+library(magrittr)
+set.seed(2)
+df<- read_csv("everthing_nona_purified.csv")
+
+df1 <- as.data.frame(df[127])
+for (i in c(5:126)) {
+  df1 <- cbind(df1, normalized(as.matrix(df[i])))
+}
+
+df1 <- df1[-c(43:53, 69, 120)]
+
+train_id <- sample(nrow(df1), floor(nrow(df1)*0.7))
+df_train <- df1[train_id,]
+df_test <- df1[-train_id,]
+Y_train = as.matrix(df_train[1])
+X_train = as.matrix(df_train[-c(1)])
+Y_test = as.matrix(df_test[1])
+X_test = as.matrix(df_test[-c(1)])
+
+glm.try = glm(cancer_true~., data=df_train, family =binomial)
+summary(glm.try)
+pfit <- glm.try %>% predict(df_test, type = "response")
+pred <- ifelse(pfit > 0.2, "TRUE", "FALSE")
+plot(Y_test, pfit)
+table(Y, pred)
+
+nothing = glm(cancer_true~1, data=df_train, family=binomial)
+pfit <- glm.try %>% predict(df_train, type = "response")
+plot(Y_train, pfit)
+summary(nothing)
+
+forwards <- step(nothing,scope=list(lower=formula(nothing),upper=formula(glm.try)), direction="forward")
+formula(forwards)
+summary(forwards)
+
+forward.lg <- glm(cancer_true~xsiblingnum + ca000_w3_2_1_ + fa001 + cc012_w3_1_ + 
+                    ge010_6 + Stock.investment + ca001_w3_2_1_ + knee.height + 
+                    ge004 + hd005_w3 + i011 + hc039_w3 + Systolic.2 + 
+                    hand.strength.test.right.2 + Water.cigarettes + breath.test.1 + 
+                    fa006 + cg003_w2_1_ + waist.circumference, data=df_train, family=binomial)
+summary(forward.lg)
+pfit <- predict(forward.lg, df_test, type = "response")
+plot(Y_test, pfit)
+pred <- ifelse(pfit>2/80, 1, 0)
+table(pred, Y_test)
+
+backwards <- step(glm.try)
+
+g <- roc(Y_test~pfit, plot=TRUE, xlab="Test_specificity", ylab="Test_sensitivity",print.auc=TRUE)
+
+
+
+
+
+
+step.model <- glm.try
+i=0
+while (i<100) {
+  i = i+1
+  step.model <- stepAIC(step.model, direction="forward", trace = FALSE)
+}
+
+coef(step.model)
+summary(step.model)
+
